7cb758690058d47151cac551cebf2f77fc9df493
MAHALAKSHMImahalakshmi <athamahalakshmi27@gmail.com>
2025-09-16 13:13:25 +0530

    security groups for the instances

--- docker.tf
+++ docker.tf
@@ -14,3 +14,30 @@ resource "aws_instance" "docker" {
      Name = "${var.project}-${var.environment}-docker"
   }
 }
+resource "aws_security_group" "allow_all_docker" {
+    name        = "allow_all_docker"
+    description = "allow all traffic"
+
+    ingress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    egress {
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    lifecycle {
+      create_before_destroy = true
+    }
+
+    tags = {
+        Name = "allow-all-docker"
+    }
+}
