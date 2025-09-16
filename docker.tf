2e37786c0631538f23f103aadff1501ce272eac1
MAHALAKSHMImahalakshmi <athamahalakshmi27@gmail.com>
2025-09-16 13:58:03 +0530

    output for verification purpose sg_id ; ERROR i deleted default subnet

--- docker.tf
+++ docker.tf
@@ -1,19 +1,3 @@
-resource "aws_instance" "docker" {
-  ami           = local.ami_id
-  #instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
-  instance_type = "t3.medium"
-  # need more for terraform
-  root_block_device {
-    volume_size = 50
-    volume_type = "gp3" # or "gp2", depending on your preference
-  }
-  user_data = file("docker.sh")
-  #iam_instance_profile = "TerraformAdmin"
-  tags = {
-     Name = "${var.project}-${var.environment}-docker"
-  }
-}
 resource "aws_security_group" "allow_all_docker" {
     name        = "allow_all_docker"
     description = "allow all traffic"
@@ -40,4 +24,23 @@ resource "aws_security_group" "allow_all_docker" {
     tags = {
         Name = "allow-all-docker"
     }
+}
+output "allow_all_docker_id" {
+  value = aws_security_group.allow_all_docker.id
+}
+resource "aws_instance" "docker" {
+  ami           = local.ami_id
+  #instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
+  instance_type = "t3.medium"
+  # need more for terraform
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3" # or "gp2", depending on your preference
+  }
+  user_data = file("docker.sh")
+  #iam_instance_profile = "TerraformAdmin"
+  tags = {
+     Name = "${var.project}-${var.environment}-docker"
+  }
 }
